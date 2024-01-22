c1e27d26fd8ba26c1f31daeaa1120e4564ddff5f
Juan Manuel Agüero <juanmaguero4@gmail.com>
2024-01-22 11:21:17 -0300

    docker finalizado

--- curso-kubernetes/mscv-usuarios/src/main/java/com/aguero/springcloud/msvc/usuarios/clients/CursoClientRest.java
+++ curso-kubernetes/mscv-usuarios/src/main/java/com/aguero/springcloud/msvc/usuarios/clients/CursoClientRest.java
@@ -1,12 +1,10 @@
 package com.aguero.springcloud.msvc.usuarios.clients;
 
-
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="msvc-cursos",url = "msvc-cursos:8002")
+@FeignClient(name="msvc-cursos",url = "${msvc.cursos.url}")
 public interface CursoClientRest {
     @DeleteMapping("/eliminar-usuario/{id}")
      void eliminarCursoUsuario(@PathVariable Long id);
